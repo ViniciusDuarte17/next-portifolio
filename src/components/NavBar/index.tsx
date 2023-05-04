adbe0aa4a1a2574f5cf7e9509afbbd1f6902c73a
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-05-04 14:18:10 -0300

    refactor: formatando o component navBar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,36 +1,30 @@
 import * as S from "./styled";
 
-type Props = { 
-  screen: number,
+type Props = {
+  screen: number;
   flexDirection?: string;
-}
+};
 
-export const NavBar = ({screen, flexDirection}: Props) => {
-  
-
+export const NavBar = ({ screen, flexDirection }: Props) => {
   return (
     <nav>
-     
-        <S.Ul screen={screen} flexDirection={flexDirection}>
-          <li>
-            <S.CustomStyleLink href={"/"}>Home</S.CustomStyleLink>
-          </li>
+      <S.Ul screen={screen} flexDirection={flexDirection}>
+        <li>
+          <S.CustomStyleLink href={"/"}>Home</S.CustomStyleLink>
+        </li>
 
-          <li>
-            <S.CustomStyleLink href={"/Sobre mim"}>Sobre mim</S.CustomStyleLink>
-          </li>
+        <li>
+          <S.CustomStyleLink href={"/Sobre mim"}>Sobre mim</S.CustomStyleLink>
+        </li>
 
-          <li>
-            <S.CustomStyleLink href={"/Portfolio"}>Portfólio</S.CustomStyleLink>
-          </li>
+        <li>
+          <S.CustomStyleLink href={"/Portfolio"}>Portfólio</S.CustomStyleLink>
+        </li>
 
-          <S.Li>
-            <S.CustomStyleLink href={"/Contato"}>Contato</S.CustomStyleLink>
-          </S.Li>
-        </S.Ul>
-      
+        <S.Li>
+          <S.CustomStyleLink href={"/Contato"}>Contato</S.CustomStyleLink>
+        </S.Li>
+      </S.Ul>
     </nav>
   );
 };
-
-
