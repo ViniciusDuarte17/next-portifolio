0bbe21b7e34caf58b7296181c1667dce0c3bd1f3
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-05-07 11:57:07 -0300

    feat- componete com informações mocadas

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import * as S from "./styled";
 
 
@@ -10,13 +11,25 @@ export const Experience = ({screen}: Props) => {
     <S.Container>
       <S.SectionRow>
         <S.InformationSection colorBorder={"rgba(123, 74, 226, 0.5)"}>
-          Programador
+          <S.ProfessionalInformation>
+            <Image width={72} height={64} src={"CodeIcon.svg"} alt="icon"/>
+            <p>3 anos como</p>
+            <h2>Programador</h2>
+          </S.ProfessionalInformation>
         </S.InformationSection>
         <S.InformationSection background="rgba(123, 74, 226, 0.1)">
-          trabalhos
+        <S.ProfessionalInformation>
+            <Image width={72} height={64} src={"ProjectsIcon.svg"} alt="icon"/>
+            <p>7 anos como</p>
+            <h2>Trabalhos</h2>
+          </S.ProfessionalInformation>
         </S.InformationSection>
         <S.InformationSection colorBorder={"rgba(123, 74, 226, 0.5)"}>
-          Desiger Ui
+        <S.ProfessionalInformation>
+            <Image width={72} height={64} src={"DesignIcon.svg"} alt="icon"/>
+            <p>4 anos como</p>
+            <h2>Designer</h2>
+          </S.ProfessionalInformation>
         </S.InformationSection>
       </S.SectionRow>
 
--- src/components/Experience/styled.ts
+++ src/components/Experience/styled.ts
@@ -22,8 +22,10 @@ export const SectionRow = styled.section`
     align-items: center;
     justify-content: space-between;
     gap: 20px;
-    margin-bottom: 5%;
-    
+
+    @media only screen and (max-width:940px) {
+        width:85%;
+    }
     @media only screen and (max-width:660px) {
         flex-direction: column;
         height: 100vh;
@@ -50,4 +52,37 @@ export const DescriptionSection = styled.section`
     background: rgba(123, 74, 226, 0.1);
     border-radius: 16px;
     margin-top: 2%;
+    @media only screen and (max-width:940px) {
+        width: 80%;
+    }
+    
+`
+
+
+export const ProfessionalInformation = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 6px;
+
+    p {
+        font-family: Raleway;
+        font-style: Regular;
+        font-size: 16px;
+        line-height: 19px;
+        line-height: 100%;
+        color:#fff;
+    }
+
+    h2 {
+        font-family: Raleway;
+        font-style: Bold;
+        font-size: 32px;
+        line-height: 38px;
+        line-height: 100%;
+        color:#fff;
+    }
 `
