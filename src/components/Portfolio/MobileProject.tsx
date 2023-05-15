d5b613b8f001036ad7d31fc6dea4f6308643b736
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-05-15 17:23:03 -0300

    feat: card para mobile

--- src/components/Portfolio/MobileProject.tsx
+++ src/components/Portfolio/MobileProject.tsx
@@ -3,36 +3,41 @@ import { useEffect, useRef, useState } from "react";
 import * as S from "./styled";
 import { Card } from "../Card";
 
-
 export const MobileProject = () => {
-    const [width, setWidth] = useState(0);
-    const carousel = useRef<HTMLDivElement>(null);
-  
-    useEffect(() => {
-      if (carousel.current) {
-        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-      }
-    }, []);
+  const [width, setWidth] = useState(0);
+  const carousel = useRef<HTMLDivElement>(null);
 
-    return (
-        <>
-        <S.ContainerCarousel>
-          <S.Carousel ref={carousel} whileTap={{ cursor: "grabbing" }}>
-            <S.InnerCarousel
-              drag="x"
-              dragConstraints={{ right: 0, left: -width }}
-              initial={{ x: 100 }}
-              animate={{ x: 0 }}
-              transition={{ duration: 1.2 }}
-            >
-              {project.map((projec) => (
-                <S.CarouselItem key={projec.id}>
-                  <Card key={projec.id} />
-                </S.CarouselItem>
-              ))}
-            </S.InnerCarousel>
-          </S.Carousel>
-        </S.ContainerCarousel>
-        </>
-    )
-}
+  useEffect(() => {
+    if (carousel.current) {
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }
+  }, []);
+
+  return (
+    <>
+      <S.ContainerCarousel>
+        <S.Carousel ref={carousel} whileTap={{ cursor: "grabbing" }}>
+          <S.InnerCarousel
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            initial={{ x: 100 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1.2 }}
+          >
+            {project.map((projec) => (
+              <S.CarouselItem key={projec.id}>
+                <Card
+                  key={projec.id}
+                  title={projec.title}
+                  description={projec.descripiton}
+                  linkProject={projec.iframeUrl}
+                  linkGitHub={projec.gitHubUrl}
+                />
+              </S.CarouselItem>
+            ))}
+          </S.InnerCarousel>
+        </S.Carousel>
+      </S.ContainerCarousel>
+    </>
+  );
+};
